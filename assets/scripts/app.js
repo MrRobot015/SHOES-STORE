2d2dbba76bd5f9f545bc0d6a5f99f8cbe2b81c1c
Mujtaba Salah <mujtabasalah07@gmail.com>
2022-01-26 04:01:23 +0200

    slider

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,10 @@
 
+//the slider script
+const wrapper = document.querySelector(".slider-wrapper")
+const menuItems = document.querySelectorAll(".menu-item")
+
+menuItems.forEach((item,index)=>{
+    item.addEventListener("click", ()=>{
+        wrapper.style.transform=`translateX(${-100 * index}vw)`
+    })
+})
