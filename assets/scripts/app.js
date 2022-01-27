96e2fad33a9dddcf2d6437270b721e75d0c85f07
Mujtaba Salah <mujtabasalah07@gmail.com>
2022-01-27 03:14:22 +0200

    final styling

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -127,3 +127,17 @@ currentProductSizes.forEach((size,index)=>{
         size.style.color="white"
     })
 })
+
+
+//payment model
+const productButton = document.querySelector(".product-button");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
